eb092bf90ed16df0d1e538e96e679bc601d3ec9a
[zhongxiang123] <2675617908@qq.com>
2023-10-10 11:31:37 +0800

    pinia优化导航数据

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,17 +1,6 @@
 <script setup>
-import {getCategoryAPI} from '@/apis/layout'
-import { onMounted, ref } from 'vue';
-const categoryList=ref([])
-
-const getCategory=async()=>{
-    const res=await getCategoryAPI()
-    console.log(res);
-    categoryList.value=res.result
-}
-
-onMounted((()=>{
-    getCategory()
-}))
+import { useCategoryStore } from '@/stores/category'
+const categoryStore=useCategoryStore()
 </script>
 
 <template>
@@ -21,7 +10,7 @@ onMounted((()=>{
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryList" :key="item.id">
+        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink to="/">{{item.name}}</RouterLink>
         </li>
 
