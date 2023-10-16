f517bb4cafde159bb4202fecb51d311ee186c6a7
[zhongxiang123] <2675617908@qq.com>
2023-10-16 10:52:27 +0800

    路由缓存问题解决

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,20 +1,21 @@
 <script setup>
-import LayoutNav from './components/LayoutNav.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
-import LayoutFooter from './components/LayoutFooter.vue'
-import LayoutFixed from './components/LayoutFixed.vue'
-import {useCategoryStore} from '@/stores/category'
-import { onMounted } from 'vue'
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
+import { useCategoryStore } from "@/stores/category";
+import { onMounted } from "vue";
 
-const categoryStore=useCategoryStore()
+const categoryStore = useCategoryStore();
 
-onMounted(()=>categoryStore.getCategory())
+onMounted(() => categoryStore.getCategory());
 </script>
 
 <template>
   <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
+  <!-- 方案1 添加key -->
   <RouterView />
   <LayoutFooter />
 </template>
