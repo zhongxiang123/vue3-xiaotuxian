3ecc52fb47865a797c609a1df658504eb1e6be7d
[zhongxiang123] <2675617908@qq.com>
2023-10-16 19:34:54 +0800

    二级-面包屑导航

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,4 +1,17 @@
-<script setup></script>
+<script setup>
+import { getCategoryFilterAPI } from "@/apis/category";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+//获取面包屑导航
+const categoryData = ref({});
+const route = useRoute();
+const getCategoryData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id);
+  categoryData.value=res.result
+};
+
+onMounted(()=>getCategoryData())
+</script>
 
 <template>
   <div class="container">
@@ -6,7 +19,7 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家 </el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{categoryData.parentName}} </el-breadcrumb-item>
         <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
