8a2c94d9db190eb36768fd6f545d688ba95a51d7
[zhongxiang123] <2675617908@qq.com>
2023-10-19 11:46:25 +0800

    登录-自定义校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,10 +2,10 @@
 // 表单校验（账户名+密码）
 import { ref } from "vue";
 // 1.准备表单对象
-
 const form = ref({
   account: "",
   password: "",
+  agree:true
 });
 // 2.准备规则对象
 const rules = ref({
@@ -29,6 +29,19 @@ const rules = ref({
       trigger: "blur",
     },
   ],
+  agree:[
+    {
+        validator:(rule,value,callback)=>{
+            console.log(value);
+            // 自定义校验逻辑（  勾选才能通过）
+            if(value){
+                callback()
+            }else{
+                callback(new Error('请勾选协议！'))
+            }
+        }
+    }
+  ]
 });
 </script>
 
@@ -65,8 +78,8 @@ const rules = ref({
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
