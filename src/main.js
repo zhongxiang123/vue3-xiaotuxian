a3d3519ae21ad6ef30e8df898309d11adf9d2e4e
[zhongxiang123] <2675617908@qq.com>
2023-10-20 11:03:05 +0800

    提交代码

--- src/main.js
+++ src/main.js
@@ -1,25 +1,24 @@
-
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import App from "./App.vue";
+import router from "./router";
 //引入初始化样式文件
-import '@/styles/common.scss'
+import "@/styles/common.scss";
 // 引入懒加载指令并且注册
-import {lazyPlugin} from '@/directives'
+import { lazyPlugin } from "@/directives";
 // 引入全局组件插件
-import {componentPlugin} from '@/components'
+import { componentPlugin } from "@/components";
+
+// 注册持久化插件
+const app = createApp(App);
+const pinia =createPinia()
+pinia.use(piniaPluginPersistedstate);
 
 
+app.use(pinia);
+app.use(router);
+app.use(lazyPlugin);
+app.use(componentPlugin);
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(lazyPlugin)
-app.use(componentPlugin)
-
-app.mount('#app')
-
+app.mount("#app");
