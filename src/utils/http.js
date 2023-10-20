3876fac614b846dfcbc9903aee25e2c5634d16e0
[zhongxiang123] <2675617908@qq.com>
2023-10-20 11:18:19 +0800

    登录-token拦截器携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,26 +1,41 @@
-import axios from 'axios'
-import { ElMessage } from 'element-plus'
+import axios from "axios";
+import { ElMessage } from "element-plus";
+import { useUserStore } from "@/stores/user";
 
 // 创建axios实例
 const httpInstance = axios.create({
-  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-  timeout: 5000
-})
+  baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
+  timeout: 5000,
+});
 
 // axios请求拦截器
-httpInstance.interceptors.request.use(config => {
-  return config
-}, e => Promise.reject(e))
+httpInstance.interceptors.request.use(
+  (config) => {
+    // 1.从pinia获取token数据
+    const userStore = useUserStore();
+
+    // 2.按照后端的要求拼接token数据
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (e) => Promise.reject(e)
+);
 
 // axios响应式拦截器
-httpInstance.interceptors.response.use(res => res.data, e => {
-  // 统一错误提示
-  ElMessage({
-    type: 'warning',
-    message:e.response.data.message
-  })
-  return Promise.reject(e)
-})
+httpInstance.interceptors.response.use(
+  (res) => res.data,
+  (e) => {
+    // 统一错误提示
+    ElMessage({
+      type: "warning",
+      message: e.response.data.message,
+    });
+    return Promise.reject(e);
+  }
+);
 
-
-export default httpInstance
+export default httpInstance;
