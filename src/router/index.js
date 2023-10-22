85ce2d471ead4c366eb40dbb9ecde552fe2d19cb
[zhongxiang123] <2675617908@qq.com>
2023-10-22 10:58:39 +0800

    购物车-基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -6,44 +6,49 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
+import CartList from '@/views/CartList/index.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: "/",
       component: Layout,
       children: [
         {
-          path: '',
-          component: Home
+          path: "",
+          component: Home,
         },
         {
-          path: 'category/:id',
-          component: Category
+          path: "category/:id",
+          component: Category,
         },
         {
-          path: 'category/sub/:id',
-          component: SubCategory
+          path: "category/sub/:id",
+          component: SubCategory,
         },
         {
-          path: 'detail/:id',
-          component: Detail
-        }
-      ]
+          path: "detail/:id",
+          component: Detail,
+        },
+        {
+          path: "cartlist",
+          component: CartList,
+        },
+      ],
     },
     {
-      path: '/Login',
-      component: Login
+      path: "/Login",
+      component: Login,
     },
   ],
   //路由滚动行为
   scrollBehavior() {
     return {
-      top: 0
-    }
-  }
-})
+      top: 0,
+    };
+  },
+});
 
 export default router
