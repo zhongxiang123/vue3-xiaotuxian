c24ed730fcbbd2ce2b6e95f900a182f7eb49b17e
[zhongxiang123] <2675617908@qq.com>
2023-10-25 15:20:16 +0800

    购物车-获取用户支付信息

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,7 +1,17 @@
 <script setup>
-const payInfo = {}
-</script>
+import { getOrderAPI } from "@/apis/pay";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 
+// 获取订单数据
+const route = useRoute();
+const payInfo = ref({});
+const getPayInfo = async () => {
+  const res = await getOrderAPI(route.query.id);
+  payInfo.value = res.result;
+};
+onMounted(() => getPayInfo());
+</script>
 
 <template>
   <div class="xtx-pay-page">
@@ -45,7 +55,6 @@ const payInfo = {}
 }
 
 .pay-info {
-
   background: #fff;
   display: flex;
   align-items: center;
@@ -121,11 +130,13 @@ const payInfo = {}
     }
 
     &.alipay {
-      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7b6b02396368c9314528c0bbd85a2e06.png) no-repeat center / contain;
+      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7b6b02396368c9314528c0bbd85a2e06.png)
+        no-repeat center / contain;
     }
 
     &.wx {
-      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c66f98cff8649bd5ba722c2e8067c6ca.jpg) no-repeat center / contain;
+      background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c66f98cff8649bd5ba722c2e8067c6ca.jpg)
+        no-repeat center / contain;
     }
   }
 }
