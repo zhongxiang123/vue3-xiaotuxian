13ce6d18ca42218fa679fb5563fce74b108fe328
[zhongxiang123] <2675617908@qq.com>
2023-10-25 16:05:18 +0800

    支付结果的展示

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,13 +39,17 @@ const router = createRouter({
           component: CartList,
         },
         {
-          path: 'checkout',
+          path: "checkout",
           component: Checkout,
         },
         {
-          path: 'pay',
-          component:Pay
-        }
+          path: "pay",
+          component: Pay,
+        },
+        {
+          path: "paycallback",
+          component: PayBack,
+        },
       ],
     },
     {
