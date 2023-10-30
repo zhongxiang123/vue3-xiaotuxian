6c5c8633a6fb22324467785d05f7bef5a56ec039
[zhongxiang123] <2675617908@qq.com>
2023-10-30 10:07:21 +0800

    订单-使用tab切换获取状态数据

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { getUserOrder } from "@/apis/order";
 import { onMounted, ref } from "vue";
-
 // tab列表
 const tabTypes = [
   { name: "all", label: "全部订单" },
@@ -12,23 +11,52 @@ const tabTypes = [
   { name: "complete", label: "已完成" },
   { name: "cancel", label: "已取消" },
 ];
-// 订单列表
+// 获取订单列表
 const orderList = ref([]);
+const total = ref(0);
 const params = ref({
   orderState: 0,
   page: 1,
   pageSize: 2,
 });
 const getOrderList = async () => {
-  const res = getUserOrder(params.value);
+  const res = await getUserOrder(params.value);
   orderList.value = res.result.items;
+  total.value = res.result.counts;
 };
+
 onMounted(() => getOrderList());
+
+// tab切换
+const tabChange = (type) => {
+  console.log(type);
+  params.value.orderState = type;
+  getOrderList();
+};
+
+// 页数切换
+const pageChange = (page) => {
+  console.log(page);
+  params.value.page = page;
+  getOrderList();
+};
+
+const fomartPayState = (payState) => {
+  const stateMap = {
+    1: "待付款",
+    2: "待发货",
+    3: "待收货",
+    4: "待评价",
+    5: "已完成",
+    6: "已取消",
+  };
+  return stateMap[payState];
+};
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane
         v-for="item in tabTypes"
@@ -73,7 +101,7 @@ onMounted(() => getOrderList());
                 </ul>
               </div>
               <div class="column state">
-                <p>{{ order.orderState }}</p>
+                <p>{{ fomartPayState(order.orderState) }}</p>
                 <p v-if="order.orderState === 3">
                   <a href="javascript:;" class="green">查看物流</a>
                 </p>
@@ -119,7 +147,13 @@ onMounted(() => getOrderList());
           </div>
           <!-- 分页 -->
           <div class="pagination-container">
-            <el-pagination background layout="prev, pager, next" />
+            <el-pagination
+              :total="total"
+              @current-change="pageChange"
+              :page-size="params.pageSize"
+              background
+              layout="prev, pager, next"
+            />
           </div>
         </div>
       </div>
